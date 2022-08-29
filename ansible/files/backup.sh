fbae794c9fba8abac37e423ae411669eed9af3be
Simon Walker <github@stwalkerster.co.uk>
2022-08-29 14:40:39 +0100

    Database server provisioning

---
+++ ansible/files/backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Managed by Ansible
+# Originally by FastLizard4, adapted by stwalkerster
+
+if [ `id -u` != 0 ]; then
+  >&2 echo 'Error: This script must be run as root.'
+  exit 1
+fi
+
+mkdir -p /media/backup/daily
+cd /media/backup/daily
+
+FILENAME="`date -u +%Y%m%dT%H%M%SZ`.sql"
+
+mysqldump --defaults-extra-file=/etc/mysql/debian.cnf --all-databases --complete-insert --disable-keys --hex-blob --quote-names --opt --single-transaction --tz-utc --flush-logs --flush-privileges --master-data=1 > "${FILENAME}"
+
+if [ $? == 0 ]; then
+  gzip -9f "${FILENAME}"
+else
+  rm "${FILENAME}"
+fi
+
+# Clean up old files
+ls -tp *.sql.gz | grep -v '/$' | tail -n +6 | xargs -d '\n' rm --
